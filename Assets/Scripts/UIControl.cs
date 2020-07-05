e5ed82524459ba38ffd6061b87d250b717e59163
Luchen Zhao <da_kids@163.com>
2020-07-05 21:59:31 +0800

    Initial commit

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIControl : MonoBehaviour
+{
+    public int a = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScalePlus();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ScalePlus()
+    {
+        GameObject.FindWithTag("ARmodels").transform.localScale += new Vector3(a,a,a);
+    }
+}
