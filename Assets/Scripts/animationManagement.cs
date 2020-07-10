e4ff2e5e5f4d37132d08a41cfc45690cf825149b
Luchen Zhao <da_kids@163.com>
2020-07-11 00:09:30 +0800

    7.11
    放大缩小

---
+++ Assets/Scripts/animationManagement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationManagement : MonoBehaviour
+{
+    private Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        Anim.speed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimScale()
+    {
+        Anim.Play("Scale", -1, 0f);
+        Anim.speed = 1f;
+    }
+    public void AnimRestore()
+    {
+        Anim.Play("Restore", -1, 0f);
+        Anim.speed = 1f;
+    }
+    public void AnimRotate()
+    {
+        Anim.Play("Rotate", -1, 0f);
+        Anim.speed = 1f;
+    }
+}
